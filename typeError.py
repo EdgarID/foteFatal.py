3b5ce7cd778cfc9f5dd128383be25ff65aea21cc
Edgar <edgardeay@gmail.com>
2025-10-06 21:12:11 +0200

    changing error name based on what we'll do

--- typeError.py
+++ typeError.py
@@ -1,9 +1,9 @@
-import wordCut as cut
-import extraGraphic as oral
-import phonoGrammic as phon
-import morphoGrammic as morph
-import logoGrammic as logo
-import ideoGrammic as ideo
+import Lettre as L
+import Form as F
+import POStagging as pos
+import Syntax as S
+import Grammar as G
+import Punctuation as P
 
 def test(file):
     txt=open(file)
@@ -17,14 +17,14 @@ def identification(file):
     two extragraphic errors and the four graphic
     one, but it's doable with any other set of
     error, based on what we'll decide."""
-    ret=[False,False,False,False,False,False]
+    ret=[0,0,0,0,0]
     txt=open(file)
-    ret[0]=cut.func(txt)
-    ret[1]=oral.func(txt)
-    ret[2]=phon.func(txt)
-    ret[3]=morph.func(txt)
-    ret[4]=logo.func(txt)
-    ret[5]=ideo.func(txt)
+    ret[0]=L.func(txt)
+    ret[1]=F.func(txt)
+    txtpos=pos.func(txt)
+    ret[2]=S.func(txtpos)
+    ret[3]=G.func(txtpos)
+    ret[4]=P.func(txtpos)
     txt.close()
     return (ret)
         
