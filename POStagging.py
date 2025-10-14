0baa32bb72295ef55888f2c8b9f8672b7a07f15e
Amjad-Ghamrawi <153656787+Amjad-Ghamrawi@users.noreply.github.com>
2025-10-14 09:17:54 +0000

    Correct POS taggimg

--- POStagging.py
+++ POStagging.py
@@ -1,7 +1,16 @@
-def func(txt):
-    """This one is for written mistakes
-    which concerne accent, double letter,
-    addition or omission of silent letter,..."""
-    
-    
-    return(True)
+import spacy
+
+nlp=spacy.load('fr_core_news_md')
+
+text = "Le petit garcon mange une pomme rouge."
+
+doc=nlp(text)
+
+for word in doc:
+    gender=word.morph.get("Gender")
+    print(f"the word is: {word.text}")
+    print(f"the lemma is: {word.lemma_}")
+    print(f"the POS tag is: {word.pos_}")
+    print(f"the features are: {word.morph}")
+    print(f"the gender is: {gender if gender else None}")
+    print('-'*30)
