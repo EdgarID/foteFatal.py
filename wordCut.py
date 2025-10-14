3e24c143229cdf627e18cd5c73b40600f0337797
Salomon Metre <kulosmetros088@gmail.com>
2025-10-14 12:30:45 +0200

    add folder for automatic POS tagging

---
+++ wordCut.py
@@ -0,0 +1,7 @@
+def func(txt):
+    """This one is for written mistakes
+    which cut word in half, merge two 
+    words together, or even add the
+    oral liaison at the beggining."""
+    
+    return(True)
